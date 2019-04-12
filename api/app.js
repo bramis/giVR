b35820dffdc5ae019e45a75578071ae008bfe01e
xDelph <thomas.delalonde@gmail.com>
2019-04-12 14:50:27 +0200

    feat(api): add transform + update mock

--- api/app.js
+++ api/app.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const axios = require("axios");
-const Unsplash = require("unsplash-js").default;
-const { toJson } = require("unsplash-js");
-const bodyParser = require("body-parser");
+const express = require('express');
+const axios = require('axios');
+const Unsplash = require('unsplash-js').default;
+const { toJson } = require('unsplash-js');
+const bodyParser = require('body-parser');
 
-global.fetch = require("node-fetch");
+global.fetch = require('node-fetch');
 
-const google_key = "AIzaSyATxSur4kjrBEg6cdNtG30zO4SIdZMgzmw";
-const google_cx = "000166153319241769584:reo5-dxx1f0";
-const searchType = "image";
+const mockGoogleCat = require('./mock/google_cat.json');
+const mockUnsplashCat = require('./mock/unsplash_cat.json');
 
-const unsplash_access_key =
-  "ecee36b14df1501d7f7740a71dc1a4d8c14655a719a25972e5d8fca0eebb14c4";
-const unsplash_secret_key =
-  "83d1b359e54f80442300c55289857dc00d43d0736e4812bcc6c2b78afdaac5dc";
+const googleKey = 'AIzaSyATxSur4kjrBEg6cdNtG30zO4SIdZMgzmw';
+const googleCx = '000166153319241769584:reo5-dxx1f0';
+const searchType = 'image';
+
+const unsplashAccessKey = 'ecee36b14df1501d7f7740a71dc1a4d8c14655a719a25972e5d8fca0eebb14c4';
+const unsplashSecretKey = '83d1b359e54f80442300c55289857dc00d43d0736e4812bcc6c2b78afdaac5dc';
 const unsplash = new Unsplash({
-  applicationId: unsplash_access_key,
-  secret: unsplash_secret_key
+  applicationId: unsplashAccessKey,
+  secret: unsplashSecretKey,
 });
 
 const app = express();
@@ -25,38 +26,62 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("hello from api");
+app.get('/', (req, res) => {
+  res.send('hello from api');
 });
 
-app.post("/google", async (req, res) => {
-  let { query } = req.body;
+const transformGoogleData = data => data.items.reduce((result, item) => {
+  result.push({
+    title: item.title,
+    src: item.link,
+    height: item.image.height,
+    width: item.image.width,
+    thumbnail: item.image.thumbnailLink,
+  });
 
-  let result =
-    query === "mock"
-      ? require("./mock/google_cat.json")
-      : (await axios.get(
-          `https://www.googleapis.com/customsearch/v1?q=${query}&searchType=${searchType}&key=${google_key}&cx=${google_cx}`
-        )).data;
+  return result;
+}, []);
 
-  res.setHeader("Content-Type", "application/json");
+const transformUnsplashData = data => data.reduce((result, item) => {
+  result.push({
+    title: item.alt_description,
+    src: item.urls.full,
+    height: item.height,
+    width: item.width,
+    thumbnail: item.urls.thumb,
+  });
+
+  return result;
+}, []);
+
+app.post('/google', async (req, res) => {
+  const { query } = req.body;
+
+  const result = query === 'mock'
+    ? mockGoogleCat
+    : transformGoogleData(
+      (await axios.get(
+        `https://www.googleapis.com/customsearch/v1?q=${query}&searchType=${searchType}&key=${googleKey}&cx=${googleCx}`,
+      )).data,
+    );
+
+  res.setHeader('Content-Type', 'application/json');
   res.json(result);
 });
 
-app.post("/unsplash", async (req, res) => {
-  let { query } = req.body;
+app.post('/unsplash', async (req, res) => {
+  const { query } = req.body;
 
-  let result =
-    query === "mock"
-      ? require("./mock/unsplash_cat.json")
-      : await unsplash.photos
-          .searchPhotos(query, undefined, 1, 10)
-          .then(toJson);
+  const result = query === 'mock'
+    ? mockUnsplashCat
+    : transformUnsplashData(
+      await unsplash.photos.searchPhotos(query, undefined, 1, 10).then(toJson),
+    );
 
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader('Content-Type', 'application/json');
   res.json(result);
 });
 
-app.listen(3000, function() {
-  console.log("api listening on port 3000!");
+app.listen(3000, () => {
+  console.log('api listening on port 3000!');
 });
